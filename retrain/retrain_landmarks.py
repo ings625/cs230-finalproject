4eb472bdbf610e597e2632ec870ea6c8fcccb726
Scott Ings <scottings@Scotts-MacBook-Pro.local>
2018-03-20 13:24:23 -0700

    save test labels to csv 2

--- retrain/retrain_landmarks.py
+++ retrain/retrain_landmarks.py
@@ -1126,7 +1126,7 @@ def run_final_eval(sess, model_info, class_count, image_lists, jpeg_data_tensor,
   csvpath = os.path.join(FLAGS.model_dir, "test_output_labels.csv") 
   with open(csvpath, 'wb') as f:
     writer = csv.writer(f)
-    writer.writerow(['filename', 'test_ground_truth', 'predictions'])  
+    # writer.writerow(['filename', 'test_ground_truth', 'predictions'])  
 
     for i, test_filename in enumerate(test_filenames):
       writer.writerow([test_filenames, test_ground_truth[i], predictions[i]])    
